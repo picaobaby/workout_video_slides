b117f77b934a47bb95d5ca42ec17057378a1bef3
Picao Baby <lioncubs11@gmail.com>
2021-07-12 07:24:26 -0400

    first commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// Custom JS code can go here
+// plyr code here ??
+
+
+// You can customize Reveal options:
+Reveal.configure({"center": false});
+
+
+// // (an outdated version of) http://headjs.com/ is also loaded
+// head.ready("lodash.min.js", function () {
+//   // LoDash can be used from here
+// });
+// head.js("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js");
